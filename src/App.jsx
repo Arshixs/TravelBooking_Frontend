1da5e23bd88415399af1f71f17f5d9aefe54671a
ArshNatani <arsh@consumableai.com>
2025-09-23 20:37:27 +0530

    Added Home Page
    added the jsx and css of the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
 
 function App() {
   return (
     <>
       <Navbar />
+      {/* <LoginPage /> */}
+      {/* <SignupPage /> */}
+      {/* <Home /> */}
     </>
   );
 }
